6290150e306b4cbadc4af90a261837b019a701e3
Sanjay Maharjan <snjmhj@gmail.com>
2015-06-04 20:27:18 +0545

    initial commit
    move to coordinate, left click and right click

---
+++ main.cpp
@@ -0,0 +1,94 @@
+#define WINVER 0x0500
+
+#include "main.h"
+
+#include <windows.h>
+#include "iostream"
+void move_cursorTo(int x, int y)
+{
+    try{
+        INPUT input={};
+        input.type = INPUT_MOUSE;
+        input.mi.mouseData=0;
+        input.mi.dx =  x*(65536/GetSystemMetrics(SM_CXSCREEN));//x being coord in pixels
+        input.mi.dy =  y*(65536/GetSystemMetrics(SM_CYSCREEN));//y being coord in pixels
+        input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+        SendInput(1,&input,sizeof(input));
+        throw true;
+    }
+    catch(bool res)
+    {
+    return !res;
+    }
+}
+
+
+void click_Left_up(int x,int y)
+{
+    INPUT input={};
+    input.type = INPUT_MOUSE;
+    input.mi.mouseData=0;
+    input.mi.dx =  x*(65536/GetSystemMetrics(SM_CXSCREEN));//x being coord in pixels
+    input.mi.dy =  y*(65536/GetSystemMetrics(SM_CYSCREEN));//y being coord in pixels
+    input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTUP;
+    SendInput(1,&input,sizeof(input));
+
+}
+void click_Left_down(int x, int y)
+{
+    INPUT input={};
+    input.type = INPUT_MOUSE;
+    input.mi.mouseData=0;
+    input.mi.dx =  x*(65536/GetSystemMetrics(SM_CXSCREEN));//x being coord in pixels
+    input.mi.dy =  y*(65536/GetSystemMetrics(SM_CYSCREEN));//y being coord in pixels
+    input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTDOWN;
+    SendInput(1,&input,sizeof(input));
+    //click_Left_up(x,y);
+}
+
+void click_Right_up(int x, int y)
+{
+    INPUT input={};
+    input.type = INPUT_MOUSE;
+    input.mi.mouseData=0;
+    input.mi.dx =  x*(65536/GetSystemMetrics(SM_CXSCREEN));//x being coord in pixels
+    input.mi.dy =  y*(65536/GetSystemMetrics(SM_CYSCREEN));//y being coord in pixels
+    input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_RIGHTUP;
+    SendInput(1,&input,sizeof(input));
+    //click_Left_up(x,y);
+}
+
+void click_Right_down(int x, int y)
+{
+    INPUT input={};
+    input.type = INPUT_MOUSE;
+    input.mi.mouseData=0;
+    input.mi.dx =  x*(65536/GetSystemMetrics(SM_CXSCREEN));//x being coord in pixels
+    input.mi.dy =  y*(65536/GetSystemMetrics(SM_CYSCREEN));//y being coord in pixels
+    input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_RIGHTDOWNDOWN;
+    SendInput(1,&input,sizeof(input));
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+    switch (fdwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+            // attach to process
+            // return FALSE to fail DLL load
+            break;
+
+        case DLL_PROCESS_DETACH:
+            // detach from process
+            break;
+
+        case DLL_THREAD_ATTACH:
+            // attach to thread
+            break;
+
+        case DLL_THREAD_DETACH:
+            // detach from thread
+            break;
+    }
+    return TRUE; // succesful
+}
---
+++ main.h
@@ -0,0 +1,33 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <windows.h>
+
+/*  To use this exported function of dll, include this header
+ *  in your project.
+ */
+
+#ifdef BUILD_DLL
+    #define DLL_EXPORT __declspec(dllexport)
+#else
+    #define DLL_EXPORT __declspec(dllimport)
+#endif
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void DLL_EXPORT move_cursorTo(int x,int y);
+void DLL_EXPORT click_Left_down(int x int y);
+void DLL_EXPORT click_Left_up(int x int y);
+void DLL_EXPORT click_Right_down(int x int y);
+void DLL_EXPORT click_Right_up(int x int y);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MAIN_H__
